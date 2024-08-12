38e02a2f3d9bd2898f3a410c614e4f20b8efc35e
Hugocl01 <hugocayon@gmail.com>
2024-08-12 02:12:42 +0200

    Eliminacion de paquetes sobrantes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://github.com/Hugocl01/hugocl01.github.io',
   base: '/',
   root: "./src",
-  integrations: [tailwind()]
+  integrations: []
 });
